5571d59ff65f1f4c4be503762bb571b5bb5d8e3c
Lume <l.niewolska@lahorde.school>
2025-03-14 12:32:52 +0100

    WIP

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -10,12 +10,14 @@ public class NPC : MonoBehaviour, IInteractable
 
     GameObject  dialoguePanel;
     TMP_Text    dialogueText;
+    GameObject  namePanel;
     TMP_Text    nameText;
     Image       portraitImage;
 
     private int lineIndex;
     private bool isTyping;
     private bool isDialogueActive;
+    private string tagDetector;
 
     [SerializeField] private PlayerMovement playerMovement;
 
@@ -24,6 +26,7 @@ public class NPC : MonoBehaviour, IInteractable
         NPCParent _parent = transform.parent.GetComponent<NPCParent>();
         dialoguePanel = _parent.GetDialoguePanel();
         dialogueText  = _parent.GetDialogueText();
+        namePanel     = _parent.GetNamePanel();
         nameText      = _parent.GetNameText();
         portraitImage = _parent.GetPortraitImage();
     }
@@ -41,18 +44,14 @@ public class NPC : MonoBehaviour, IInteractable
             return;
         }
 
-        if (isDialogueActive)
-        {
-            NextLine();
-        }
-        else
-        {
-            StartDialogue();
-        }
+        if (isDialogueActive) { NextLine(); }
+
+        else                  { StartDialogue(); }
     }
 
     void StartDialogue()
     {
+        CheckPortraitPosition();
 
         isDialogueActive = true;
         lineIndex = 0;
@@ -61,28 +60,49 @@ public class NPC : MonoBehaviour, IInteractable
         portraitImage.sprite = dialogueData.npcPortrait;
 
         dialoguePanel.SetActive(true);
-        // * Disable Player Movement
 
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
-        //FOR FUTURE CHARACTER COLOR: https://discussions.unity.com/t/fixed-change-color-of-individual-characters-in-textmeshpro-text-ui/880934/2
-
+        //Stop player momvement
         yield return new WaitForEndOfFrame();
-        Debug.Log("End of frame");
         if (playerMovement) playerMovement.StopPlayerMovement();
 
+        //Clear the dialogue text and start typing
         isTyping = true;
         dialogueText.SetText("");
 
+        //Parse the text
         foreach(char letter in dialogueData.dialogueLines[lineIndex])
         {
+            //Check for tags
+            if (letter == '$')
+            {
+                tagDetector += letter;
+                Debug.Log("Dialogue tag detected");
+                yield return new WaitForEndOfFrame();
+            }
+
+            //If it's a tag tag, check what comes next
+            if (tagDetector != "")
+            {
+                if (letter == ' ')
+                {
+                    tagDetector = "";
+                    yield return new WaitForEndOfFrame();
+                }
+
+                tagDetector += letter;
+            }
+
+            //Add the letter and wait
             dialogueText.text += letter;
             yield return new WaitForSeconds(dialogueData.talkingSpeed);
         }
 
+        //Stop typing
         isTyping = false;
     }
 
@@ -107,11 +127,31 @@ public class NPC : MonoBehaviour, IInteractable
         isTyping = false;
         dialogueText.SetText("");
         dialoguePanel.SetActive(false);
-        // * Enable Player Movement
     }
 
     public void SetPlayerReference(GameObject _player)
     {
         playerMovement = _player.GetComponent<PlayerMovement>();
+    }
+
+    void CheckPortraitPosition()
+    {
+        if (portraitImage.transform.localPosition.x > 0) //if the portrait is to the LEFT of the dialogue box
+        {
+            if (dialogueData.isPortraitOnTheRight) { SwapPortraitSide(); }
+            return;
+        }
+        else                                          //if the portrait is to the RIGHT of the dialogue box
+        {
+            if (!dialogueData.isPortraitOnTheRight) { SwapPortraitSide(); }
+            return;
+        }
+    }
+
+    private void SwapPortraitSide()
+    {
+        dialogueText.transform.localPosition  = new Vector3(-dialogueText.transform.localPosition.x,  dialogueText.transform.localPosition.y);
+        namePanel.transform.localPosition     = new Vector3(-namePanel.transform.localPosition.x,     namePanel.transform.localPosition.y);
+        portraitImage.transform.localPosition = new Vector3(-portraitImage.transform.localPosition.x, portraitImage.transform.localPosition.y);
     }
 }
--- Assets/Scripts/NPCParent.cs
+++ Assets/Scripts/NPCParent.cs
@@ -6,11 +6,18 @@ public class NPCParent : MonoBehaviour
 {
     [SerializeField] GameObject dialoguePanel;
     [SerializeField] TMP_Text dialogueText;
+    [SerializeField] GameObject namePanel;
     [SerializeField] TMP_Text nameText;
     [SerializeField] Image portraitImage;
 
+    [SerializeField] Color nameColor;
+    [SerializeField] Color placeColor;
+    [SerializeField] Color itemColor;
+    [SerializeField] Color buttonColor;
+
     public GameObject GetDialoguePanel() { return dialoguePanel; }
-    public TMP_Text   GetDialogueText()  { return dialogueText; }
-    public TMP_Text   GetNameText()      { return nameText; }
+    public TMP_Text   GetDialogueText()  { return dialogueText;  }
+    public GameObject GetNamePanel()     { return namePanel;     }
+    public TMP_Text   GetNameText()      { return nameText;      }
     public Image      GetPortraitImage() { return portraitImage; }
 }
