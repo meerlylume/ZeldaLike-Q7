8243f5157daa3ac9bc2115af0b37facfff2ce789
Lume <l.niewolska@lahorde.school>
2025-05-07 14:27:39 +0200

    Fix-Inventory

--- Assets/Scripts/Quests/QuestTracker.cs
+++ Assets/Scripts/Quests/QuestTracker.cs
@@ -25,13 +25,22 @@ public class QuestTracker : MonoBehaviour
 
     private void BuildLists()
     {
-        if (allPanels.Count == quests.Count) return;
-
         if (allPanels.Count > quests.Count)
         {
             Debug.LogError("Error Building Quest List: allPanels.Count > quests.Count");
             return;
         }
+
+        for (int i = 0; i < allPanels.Count; i++)
+        {
+            if (quests[i].IsCompleted())
+            {
+                allPanels[i].transform.SetParent(completedGrid.transform);
+                allPanels[i].transform.localScale = Vector3.one;
+            }
+        }
+
+        if (allPanels.Count == quests.Count) return;
 
         for (int i = allPanels.Count; i < quests.Count; i++)
         {
@@ -45,7 +54,7 @@ public class QuestTracker : MonoBehaviour
                 completedScript.SetQuestDesc(quests[i].GetDescription());
                 completedScript.SetQuestName(quests[i].GetName());
 
-                completedObject.transform.localScale = new Vector3(1, 1, 1);
+                completedObject.transform.localScale = Vector3.one;
 
                 allPanels.Add(completedObject);
             }
@@ -60,7 +69,7 @@ public class QuestTracker : MonoBehaviour
                 progressScript.SetQuestName(quests[i].GetName());
                 progressScript.SetRewardsDesc(quests[i].GetRewardsDesc());
 
-                progressObject.transform.localScale = new Vector3(1, 1, 1);
+                progressObject.transform.localScale = Vector3.one;
 
                 allPanels.Add(progressObject);
             }
